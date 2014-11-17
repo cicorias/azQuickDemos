b14ceb6a45b7f16d3cb9fb208e0dd63ee3a9cae6
Shawn Cicoria <shawn@cicoria.com>
2014-11-17 14:18:11 -0500

    Adding HDInsight stuff

---
+++ hdinsight/Runit.ps1
@@ -0,0 +1,18 @@
+﻿
+Add-AzureAccount
+
+Select-AzureSubscription -SubscriptionName "Microsoft Azure Internal Consumption"
+
+$strg = "scicoriahdp2"
+$container = "hdproot"
+$clusterName = "scicoriahdp2cluster"
+$location = "East US"
+
+$strgAccount = New-AzureStorageAccount -StorageAccountName $strg -Location $location
+
+Set-AzureStorageAccount -StorageAccountName $strg -GeoReplicationEnabled $false
+
+
+.\New-HDInsightCluster.ps1 -Cluster $clusterName -Location $location  `
+      -DefaultStorageAccount $strg -DefaultStorageContainer $container `
+      -ClusterSizeInNodes 1
