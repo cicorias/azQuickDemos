9c5d7367a9074f04c901571479879680c17bfee2
Shawn Cicoria <shawn@cicoria.com>
2014-12-04 13:40:58 -0500

    updates

---
+++ hdinsight/RunQuery.ps1
@@ -0,0 +1,8 @@
+﻿
+
+#$creds = Get-Credential
+
+Invoke-RestMethod https://scicoriahdp2cluster.azurehdinsight.net/hbaserest -Credential $creds
+
+$row = Invoke-RestMethod https://scicoriahdp2cluster.azurehdinsight.net/hbaserest/tweets_by_words/row1 -Credential $creds
+$row.CellSet.Row.Cell
